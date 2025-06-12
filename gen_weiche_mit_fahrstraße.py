b95eb4bf44b562488b1f6804996d87f358c72c3d
fidoriel <49869342+fidoriel@users.noreply.github.com>
2025-06-12 12:45:57 +0200

    working train

--- gen_weiche_mit_fahrstraße.py
+++ gen_weiche_mit_fahrstraße.py
@@ -1,11 +1,19 @@
+import interlocking
 from yaramo.model import Edge, Node, Topology
-
+from time import sleep
 from yaramo.model import Edge, EuclideanGeoNode, Node
 from yaramo.route import Route
 from yaramo.signal import SignalFunction, Signal, SignalDirection, SignalKind
 from railwayroutegenerator.routegenerator import RouteGenerator
 from sumoexporter import SUMOExporter
 from yaramo.operations import Split
+from interlocking.infrastructureprovider import (
+    LoggingInfrastructureProvider,
+    SUMOInfrastructureProvider,
+)
+import asyncio
+import traci
+from interlocking.interlockinginterface import Interlocking
 
 
 def create_geo_node(x, y):
@@ -27,13 +35,13 @@ def create_simple_weiche():
     end_a = Node(name="a", geo_node=create_geo_node(0, 0))
     topology.add_node(end_a)
 
-    end_b = Node(name="b", geo_node=create_geo_node(150, 0))
+    end_b = Node(name="b", geo_node=create_geo_node(1500, 0))
     topology.add_node(end_b)
 
-    end_c = Node(name="c", geo_node=create_geo_node(150, 50))
+    end_c = Node(name="c", geo_node=create_geo_node(1500, 50))
     topology.add_node(end_c)
 
-    weiche = Node(name="w", geo_node=create_geo_node(50, 0))
+    weiche = Node(name="w", geo_node=create_geo_node(500, 0))
     topology.add_node(weiche)
 
     a2weiche = create_edge(end_a, weiche)
@@ -42,44 +50,45 @@ def create_simple_weiche():
     weiche2b = create_edge(weiche, end_b)
     topology.add_edge(weiche2b)
 
-    geo_node_weiche2c = create_geo_node(100, 50)
+    geo_node_weiche2c = create_geo_node(1000, 50)
     weiche2c = create_edge(weiche, end_c, inter_geo_nodes=[geo_node_weiche2c])
     topology.add_edge(weiche2c)
 
     signal_a = Signal(
-        name="sa",
+        name="SA",
         edge=a2weiche,
         direction=SignalDirection.IN,
         kind=SignalKind.Hauptsignal,
         function=SignalFunction.Block_Signal,
-        distance_edge=1.0,
+        distance_edge=100.0,
     )
     topology.add_signal(signal_a)
     a2weiche.signals.append(signal_a)
 
     signal_b = Signal(
-        name="sb",
+        name="SB",
         edge=weiche2b,
         direction=SignalDirection.IN,
         kind=SignalKind.Hauptsignal,
         function=SignalFunction.Block_Signal,
-        distance_edge=1.0,
+        distance_edge=100.0,
         additional_signals=[signal_a],
     )
     topology.add_signal(signal_b)
     weiche2b.signals.append(signal_b)
 
     signal_c = Signal(
-        name="sc",
+        name="SC",
         edge=weiche2c,
         direction=SignalDirection.IN,
         kind=SignalKind.Hauptsignal,
         function=SignalFunction.Block_Signal,
-        distance_edge=1.0,
+        distance_edge=100.0,
         additional_signals=[signal_a],
     )
     topology.add_signal(signal_c)
     weiche2c.signals.append(signal_c)
+    topology.update_edge_lengths()
 
     print(topology.nodes)
     print(topology.edges)
@@ -91,6 +100,28 @@ def create_simple_weiche():
     sumo_exporter = SUMOExporter(topology)
     sumo_exporter.convert()
     sumo_exporter.write_output()
+
+    traci.init(port=9090)
+    # traci.start(["sumo-gui", "--configuration-file", "sumo-config/weiche.scenario.sumocfg"], port=9090, verbose=True)
+    traci.setOrder(1)
+    interlocking = Interlocking(
+        [
+            SUMOInfrastructureProvider(traci_instance=traci),
+            LoggingInfrastructureProvider(),
+        ]
+    )
+    interlocking.prepare(topology)
+
+    train_id = "TRAIN_1"
+    traci.vehicle.add(train_id, f"route_{signal_a.name}-{signal_b.name}", "regio")
+    route = list(topology.routes.values())[0]
+    route.maximum_speed = 50
+    asyncio.run(interlocking.set_route(yaramo_route=route, train_id=train_id))
+    # traci.vehicle.setSpeedMode(train_id, 32)
+
+    while True:
+        traci.simulationStep()
+        sleep(1)
 
 
 if __name__ == "__main__":
